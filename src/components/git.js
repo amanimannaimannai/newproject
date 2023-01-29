9ab9dc605b849fb084e60e964971b9867db0bad4
amani mannai <amanimannai2311@gmail.com>
2023-01-29 14:25:09 +0100

    modifying 1

---
+++ src/components/git.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { FaTrash } from "react-icons/fa";
+ const  Githubpage = (props)=>{
+   
+    return(
+        
+          <div className='item'>
+                <h1>hello git</h1>
+
+            
+            </div>
+        
+    )
+ }
+ export default Githubpage
